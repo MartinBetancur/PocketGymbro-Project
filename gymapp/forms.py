477aa310aca23c44234f7d625512090f6097e818
JeronimoV <jeronimo.velasquez.escobar@gmail.com>
2024-04-23 17:16:27 -0500

    Gestion de usuarios funcional

--- gymapp/forms.py
+++ gymapp/forms.py
@@ -2,7 +2,8 @@ from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from .models import Perfil
+from .models import Perfil, Equipamiento_Del_Usuario
+from django.forms import ModelForm
 
 
 class SignUpForm(UserCreationForm):
@@ -13,3 +14,12 @@ class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+class PerfilForm(ModelForm):
+    class Meta:
+        model = Perfil
+        fields = ['nombre','apellido','peso','altura','condicion_fisica','fecha_Nacimiento','objetivos','genero','deporte_practicado','condiciones_medicas']
+
+class EquipamientoForm(ModelForm):
+    class Meta:
+        model = Equipamiento_Del_Usuario
+        fields = ['equp_gimnasio', 'equp_casa']
--- pocketgymbro/settings.py
+++ pocketgymbro/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = '/accounts/login/'
+
 
 
 # Default primary key field type

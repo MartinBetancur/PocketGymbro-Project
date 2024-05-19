3f6b9c9ed6f73d397debb31e50660fe1390269f0
JeronimoV <jeronimo.velasquez.escobar@gmail.com>
2024-05-19 12:38:28 -0500

    Macros, mejora en los modelos, Modelo de rutinas y dietas semanales creados

--- gymapp/admin.py
+++ gymapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import  Perfil, Historial, DietaDiaria, Equipamiento_Del_Usuario, Lesiones
+from .models import  Perfil, Historial, DietaDiaria, Equipamiento_Del_Usuario, Lesiones, Macros
 
 admin.site.register(Perfil)
 admin.site.register(Historial)
 admin.site.register(DietaDiaria)
 admin.site.register(Equipamiento_Del_Usuario)
 admin.site.register(Lesiones)
+admin.site.register(Macros)
--- gymapp/models.py
+++ gymapp/models.py
@@ -18,6 +18,67 @@ class CondicionFisica(models.IntegerChoices):
 
 
 class Perfil(models.Model):
+    DEPORTE_CHOICES = [
+        ('SOCCER', 'Soccer'),
+        ('BASKETBALL', 'Basketball'),
+        ('VOLLEYBALL', 'Volleyball'),
+        ('UNDERWATER HOCKEY', 'Underwater Hockey'),
+        ('TENNIS', 'Tennis'),
+        ('TABLE TENNIS', 'Table Tennis'),
+        ('ATHLETICS', 'Athletics'),
+        ('CYCLING', 'Cycling'),
+        ('CALISTHENICS ', 'Calisthenics'),
+        ('HOCKEY ', 'Hockey'),
+        ('ARCHERY ', 'Archery'),
+        ('OTHER', 'Other'),
+        
+    ]
+    OBJETIVOS_CHOICES = [
+        ('MAXIMUM FAT LOSS','Maximum Fat Loss'),
+        ('SOME FAT LOSS','Some Fat Loss'),
+        ('MANTAIN','Maintain'),
+        ('STEADY GAIN','Steady Gain'),
+        ('GAIN','Gain'),
+    ]
+    GENERO_CHOICES = [
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+        ('NON_BINARY', 'Non-binary'),
+        ('TRANSGENDER', 'Transgender'),
+        ('GENDERQUEER', 'Genderqueer'),
+        ('GENDERFLUID', 'Genderfluid'),
+        ('AGENDER', 'Agender'),
+        ('BIGENDER', 'Bigender'),
+        ('TWO_SPIRIT', 'Two-spirit'),
+        ('OTHER', 'Other'),
+        ('CISGENDER', 'Cisgender'),
+        ('GENDER_QUESTIONING', 'Gender questioning'),
+        ('GENDER_NONCONFORMING', 'Gender nonconforming'),
+        ('PANGENDER', 'Pangender'),
+        ('DEMIBOY', 'Demiboy'),
+        ('DEMIGIRL', 'Demigirl'),
+        ('ANDROGYNE', 'Androgyne'),
+        ('NEUTROIS', 'Neutrois'),
+        ('GENDERFLUX', 'Genderflux'),
+        ('THIRD_GENDER', 'Third gender'),
+        ('BI_GENDER', 'Bi-gender'),
+        ('GENDER_EXPANSIVE', 'Gender expansive'),
+        ('GENDER_VARIANT', 'Gender variant'),
+        ('INTERSEX', 'Intersex'),
+        ('THIRD_SEX', 'Third sex'),
+        ('MAHORAGA', 'Mahoraga'),
+        ('ITACHI', 'Itachi'),
+        ('BOEING_AH_64_APACHE', 'Boeing AH-64 Apache'),
+        ('GOKU', 'Goku'),
+        ('DECEPTICON', 'Decepticon'),
+        ('OPTIMUS_PRIME', 'Optimus prime'),
+        ('PERRO', 'Perro'),
+        ('GATO', 'Gato'),
+        ('OBAMA', 'Obama'),
+        ('NONE', 'None'),
+        ('PREFER_NOT_TO_SAY', 'Prefer Not to Say'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.TextField(default=None)
     apellido = models.TextField(default='', null=True)
@@ -31,9 +92,9 @@ class Perfil(models.Model):
         return edad
 
     edad = property(calcular_edad)
-    objetivos = models.TextField(default=None)
-    genero = models.TextField(default=None)
-    deporte_practicado = models.TextField(default=None)
+    objetivos = models.CharField(max_length=21, choices=OBJETIVOS_CHOICES, default='MANTAIN')
+    genero = models.CharField(max_length=20, choices=GENERO_CHOICES, default='NONE')
+    deporte_practicado = models.CharField(max_length=21, choices=DEPORTE_CHOICES, default='OTROS')
     fecha_de_registro = models.DateField(default=None)
     condiciones_medicas = models.TextField(default=None)
 
@@ -51,12 +112,12 @@ class Historial(models.Model):
     
 class DietaDiaria(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comida1 = models.TextField(default=None)
-    comida2 = models.TextField(default=None)
-    comida3 = models.TextField(default=None)
-    comida4 = models.TextField(default=None)
-    comida5 = models.TextField(default=None)
-    comida6 = models.TextField(default=None)
+    comida1 = models.CharField(default=None, max_length= 21)
+    comida2 = models.CharField(default=None, max_length= 21)
+    comida3 = models.CharField(default=None, max_length= 21)
+    comida4 = models.CharField(default=None, max_length= 21)
+    comida5 = models.CharField(default=None, max_length= 21)
+    comida6 = models.CharField(default=None, max_length= 21)
     fecha = models.DateField(default=None)
 
 class Equipamiento_Del_Usuario(models.Model):
@@ -67,3 +128,32 @@ class Equipamiento_Del_Usuario(models.Model):
 class Lesiones(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     lesion = models.TextField(default=None)
+
+
+
+class Macros(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    calorias = models.DecimalField(max_digits=10, decimal_places=2)
+    proteinas = models.DecimalField(max_digits=10, decimal_places=2)
+    grasas = models.DecimalField(max_digits=10, decimal_places=2)
+    carbohidratos = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Dieta_Semanal(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lunes = models.JSONField()
+    martes = models.JSONField()
+    miercoles = models.JSONField()
+    jueves = models.JSONField()
+    viernes = models.JSONField()
+    sabado = models.JSONField()
+    domingo = models.JSONField()
+
+class Rutina_Semanal(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lunes = models.JSONField()
+    martes = models.JSONField()
+    miercoles = models.JSONField()
+    jueves = models.JSONField()
+    viernes = models.JSONField()
+    sabado = models.JSONField()
+    domingo = models.JSONField()
